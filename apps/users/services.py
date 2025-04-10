38c55341f8546cfaad7b0ca45bdced55423d6f20
dimash <arystambekdimash005@gmail.com>
2025-04-10 23:17:35 +0500

    feat: set password was correct

--- apps/users/services.py
+++ apps/users/services.py
@@ -1,8 +1,11 @@
 class UserService:
     @staticmethod
     def update(user, data: dict):
+        password = data.pop('password')
         for k, v in data.items():
             setattr(user, k, v)
 
+        if password:
+            user.set_password(password)
         user.save()
         return user
