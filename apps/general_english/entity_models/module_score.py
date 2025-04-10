1cc1f56f1a1966332307b0e954f0fceb263c86f3
dimash <arystambekdimash005@gmail.com>
2025-04-10 23:13:35 +0500

    feat: module score has changed score to none from 0

--- apps/general_english/entity_models/module_score.py
+++ apps/general_english/entity_models/module_score.py
@@ -7,6 +7,6 @@ from apps.general_english import models as general_english_models
 class ModuleScore(models.Model):
     module = models.ForeignKey(general_english_models.Module, on_delete=models.CASCADE)
     section = models.CharField(choices=enums.ModuleSectionType.choices, max_length=10)
-    score = models.FloatField(default=0)
+    score = models.FloatField(default=None, null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
