adbdd13ef44872352b91e13b50b4a413723e6bf9
dimash <arystambekdimash005@gmail.com>
2025-04-11 00:12:23 +0500

    feat: fixed get image url

--- apps/common/utils.py
+++ apps/common/utils.py
@@ -1,24 +1,14 @@
-import re
-
 import requests
 from bs4 import BeautifulSoup
 
 
 def get_image_url(image_query: str) -> str | None:
     search_url = f"https://www.google.com/search?q={image_query}&tbm=isch"
-    headers = {
-        "User-Agent": "Mozilla/5.0"
-    }
-
-    response = requests.get(search_url, headers=headers)
+    response = requests.get(search_url)
     soup = BeautifulSoup(response.text, "html.parser")
-
-    scripts = soup.find_all("script")
-
-    for script in scripts:
-        if 'AF_initDataCallback' in script.text:
-            matches = re.findall(r'"(https://[^"]+\.(?:jpg|jpeg|png))"', script.text)
-            if matches:
-                return matches[0]
-
-    return None
+    img_tag = soup.find("img", {"class": "yWs4tf"})
+    if img_tag is not None:
+        img_link = img_tag.get("src")
+        return img_link
+    else:
+        print("No image found on the page.")
