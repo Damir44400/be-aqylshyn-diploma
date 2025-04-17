198e4e69fbcf58df6bf602e7130657b60e101707
dimash <arystambekdimash005@gmail.com>
2025-04-18 02:12:56 +0500

    ref: fixed ielts speaking model

--- apps/ielts/entity_models/ielts_speaking.py
+++ apps/ielts/entity_models/ielts_speaking.py
@@ -5,12 +5,12 @@ from apps.ielts.entity_models.ielts_test import IeltsTest
 
 class IeltsSpeakingQuestion(models.Model):
     question = models.TextField()
-    additional_information = models.TextField()
+    additional_information = models.TextField(null=True, blank=True)
 
     test = models.ForeignKey(IeltsTest, on_delete=models.CASCADE, related_name='speakings')
 
     def __str__(self):
-        return self.test
+        return f"{self.question} {self.test}"
 
     class Meta:
         verbose_name_plural = "Speaking | Tasks"
