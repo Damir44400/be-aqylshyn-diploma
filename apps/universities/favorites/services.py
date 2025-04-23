b9c34c66b7083cdf4addabcc8c74b7f848322824
dimash <arystambekdimash005@gmail.com>
2025-04-24 00:32:20 +0500

    ref: fixed inserting favorite to db

--- apps/universities/favorites/services.py
+++ apps/universities/favorites/services.py
@@ -1,15 +1,8 @@
-from rest_framework.exceptions import ValidationError
-
-from apps.universities import models
 from apps.universities.favorites import models as favorites_models
 
 
 class FavoriteService:
-    def insert_favorite(self, university_id, user):
-        university = models.University.objects.filter(id=university_id).first()
-        if not university:
-            raise ValidationError("No such university")
-
+    def insert_favorite(self, university, user):
         favorite = favorites_models.Favorite.objects.filter(university=university, user=user).first()
         if not favorite:
             favorites_models.Favorite.objects.create(university=university, user=user)
