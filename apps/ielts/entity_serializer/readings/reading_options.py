0fe0ebb53ab35d9e44b1a5f722772e4b1c3ede5f
dimash <arystambekdimash005@gmail.com>
2025-06-12 08:56:26 -0600

    feat(reading): added is correct field to reading option

--- apps/ielts/entity_serializer/readings/reading_options.py
+++ apps/ielts/entity_serializer/readings/reading_options.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 class ReadingOptionsSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     option = serializers.CharField()
+    is_correct = serializers.BooleanField()
 
 
 class ReadingSelectInsertSerializer(serializers.Serializer):
